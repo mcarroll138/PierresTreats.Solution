dc0d0fdb991e13f10119b46098682484456630cc
Michael Carroll <mcarroll138@gmail.com>
2023-08-11 12:37:05 -0700

    udate Treat model, include description, retest CRUD after migration

---
+++ PierresTreats/Migrations/20230811192407_TreatDescription.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PierresTreats.Migrations
+{
+    public partial class TreatDescription : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "Treats",
+                type: "longtext",
+                nullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "Treats");
+        }
+    }
+}
--- PierresTreats/Models/Treat.cs
+++ PierresTreats/Models/Treat.cs
@@ -8,6 +8,7 @@ namespace PierresTreats.Models
     {
         public int TreatId { get; set; }
         public string Name { get; set; }
+        public string Description { get; set; }
 
         public List<FlavorTreat> JoinEntities { get; }
         public ApplicationUser User { get; set; }
