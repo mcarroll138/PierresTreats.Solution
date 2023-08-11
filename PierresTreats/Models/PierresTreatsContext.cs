1c7824100075b7c2944af4a207239736a5609321
Michael Carroll <mcarroll138@gmail.com>
2023-08-11 15:02:55 -0700

    debug Authorization

--- PierresTreats/Models/PierresTreatsContext.cs
+++ PierresTreats/Models/PierresTreatsContext.cs
@@ -1,8 +1,9 @@
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 
 namespace PierresTreats.Models
 {
-    public class PierresTreatsContext : DbContext
+    public class PierresTreatsContext : IdentityDbContext<ApplicationUser>
     {
         public DbSet<Treat> Treats { get; set; }
         public DbSet<Flavor> Flavors { get; set; }
